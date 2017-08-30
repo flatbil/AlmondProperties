af671164f8991abf1b0b471c69b5e97fdad43d1c
William Almond <walmond@gmail.com>
2017-08-30 11:58:02 -0700

    Initial Commit of files

---
+++ actions.js
@@ -0,0 +1,20 @@
+/*
+author: William Almond
+site: almondproperties.com
+org: Nuthouse Web Solutions, 2016
+*/
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}
+    x[myIndex-1].style.display = "block";
+    setTimeout(carousel, 9000);
+}
